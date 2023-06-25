8f9d04adebc2efa360f2f41e40244b8f054e3105
Jishnu Purahoo <pura0004@algonquinlive.com>
2023-06-25 12:28:44 -0400

    script nav js modified

--- script.js
+++ script.js
@@ -10,3 +10,27 @@ function mobileMenu() {
   navMenu.classList.toggle("active");
   navClose.classList.toggle("active");
 }
+
+
+// closing nav bar 
+const closeNav = document.querySelectorAll(".close");
+
+closeNav.forEach((n) => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}
+
+
+// Close menu when click on cross
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach((n) => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+  hamburger.classList.remove("active");
+  navClose.classList.remove("active");
+  navMenu.classList.remove("active");
+}
+
